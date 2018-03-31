8c8141504eee09e516a98674b330adb7d1013cee
joshinils <34302971+joshinils@users.noreply.github.com>
2018-03-31 11:23:54 +0200

    pre-m

--- main.cpp
+++ main.cpp
@@ -14,11 +14,27 @@ namespace std
 	{
 		size_t operator()(const vector<collatzType>& v) const
 		{ 
+//			cout << " VecHash["<<std::hash<collatzType>()(v.at(0))<<"] ";
 			// only the first counts, which is the number, not the second which is the count
 			return std::hash<collatzType>()(v.at(0)) ;
 		}
 	};
 }
+
+template <typename T>
+inline bool operator == (const vector<T>& vL, const vector<T>& vR)
+{	return vL.at(0) == vR.at(0);	}
+
+template <>
+inline bool operator == (const vector<unsigned long long>& vL, const vector<unsigned long long>& vR)
+{	return vL.at(0) == vR.at(0);	}
+
+template <typename T>	inline T divideMeBy2(T& t)	{	return t/2;	}
+template <>	inline BigInt divideMeBy2<BigInt>(BigInt& bi)	{	return bi.divideMeBy2();	}
+
+template <typename T>	inline bool isEven(T& t)	{	return t%2==0;	}
+template <>	inline bool isEven<BigInt>(BigInt& bi)	{	return bi.isEven();	}
+
 
 ostream& operator << (ostream& ostr, vector<collatzType> v)
 {
@@ -28,17 +44,8 @@ ostream& operator << (ostream& ostr, vector<collatzType> v)
 	return 	ostr<<"\b\b]";
 }
 
-bool operator == (const vector<collatzType>& vL, const vector<collatzType>& vR)
-{	return vL.at(0) == vR.at(0);	}
 
 static unordered_set<vector<collatzType>> lookUpTable;
-
-template <typename T>	inline T divideMeBy2(T& t)	{	return t/2;	}
-template <>	inline BigInt divideMeBy2<BigInt>(BigInt& bi)	{	return bi.divideMeBy2();	}
-
-template <typename T>	inline bool isEven(T& t)	{	return t%2==0;	}
-template <>	inline bool isEven<BigInt>(BigInt& bi)	{	return bi.isEven();	}
-
 collatzType collatz(collatzType arg)//, collatzType count=0)
 {
 	cout << "		 cARG: "<<arg<<endl;
@@ -50,12 +57,12 @@ collatzType collatz(collatzType arg)//, collatzType count=0)
 //	cout << "arg: "<<arg << " c: "<<count << endl;
 
 	if(arg < 1)
-		return -1;
+		return 0;
 
 
 	vector<collatzType> tableEntry(2);
 	tableEntry.at(0)=arg;
-	tableEntry.at(1)=collatzType(-1); // we dont know this yet
+	tableEntry.at(1)=collatzType(0); // we dont know this yet
 
 	// base case
 	if(arg==1)
@@ -119,18 +126,41 @@ void printLUT()
 	cout << "	LUT:";
 	for (auto& it:lookUpTable)
 		cout << it;
-	cout << endl;	
+	cout <<" size: "<<lookUpTable.size()<< endl;	
 }
 
 int main()
 {
+	/*
+	vector<collatzType> vL(2);
+	vL.at(0)=1;
+	vL.at(1)=2;
+	
+	vector<collatzType> vR(2);
+	vR.at(0)=1;
+	vR.at(1)=4;
+
+	cout << vL << vR << " ; " << (vL==vR) << endl;
+
+	
+	lookUpTable.insert(vL);
+	cout<< "inserted " <<vL<<endl;
+	unordered_set<vector<collatzType>>::const_iterator itFind = lookUpTable.find(vR);
+	if(itFind==lookUpTable.end())
+		cout << "didnt find "<<vR<<" -------------"<<endl;
+	else
+		cout << "found it!++++++++++++++++++++++++"<<(*itFind).at(0)<<" ; "<<(*itFind).at(1)<<endl;
+	printLUT(); 
+
+	return 0; //*/
+
 	lookUpTable.rehash(1024); // more buckets = more better, more faster
 
 	collatzType longest=0;
 	int everyNth =1;//50000;
 	int counter=0;
 	bool newLonger=false;
-	collatzType start(0);
+	collatzType start(1);
 	collatzType end(10);
 //	collatzType start(11200681);//LLONG_MAX); // 11200681 is the current best/longest
 //	collatzType end = LLONG_MAX ;//start + 10000;
